c39300c4e92645f6ccf30c096e3580d596738b84
Juan_Carlos_Aleman <jaleman@cgclatam.com>
2021-11-17 18:04:19 -0600

    Se cambian archivos de jsx a tsx

--- src/components/layout/Layouts.tsx
+++ src/components/layout/Layouts.tsx
@@ -1,25 +1,31 @@
-import React,{ useState } from "react";
+import React,{ FunctionComponent, ReactChild, ReactChildren, ReactNode, useState } from "react";
 import { Layout } from 'antd';
 import { 
     MenuUnfoldOutlined,
     MenuFoldOutlined
  } from '@ant-design/icons';
-import HeaderDiv  from './HeaderDiv';
+
 import MenuLayout  from './MenuLayout';
+import HeaderDiv from "./HeaderDiv";
 
-export default function Layouts(props){
 
+type LayoutProps = {
+    children?: JSX.Element[] | JSX.Element;
+    title?: string;
+    classeName?: string;
+ }
+
+const Layouts:React.FC<LayoutProps> = (props: LayoutProps) => {
+    const { children, title,classeName } = props;
     const { Header,Sider, Content , Footer} = Layout;
     const [collapsed, setCollapsed] =  useState(true);
 
     const toggle = () =>  setCollapsed(!collapsed);
-
     return(
-        <Layout className={`${ props.classname }`} >
+        <Layout className={`${ classeName }`} >
             <Sider style={{ background: '#fff' }} className="sidebar-left"  trigger={null} collapsible collapsed={collapsed}> 
               <div className="logo" />
                 <MenuLayout collapsed={collapsed} onToggle={() => { setCollapsed(!collapsed) }} />
-               
                 {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
                     className: 'trigger',
                     onClick: toggle,
@@ -30,7 +36,7 @@ export default function Layouts(props){
                     <HeaderDiv onToggle={toggle} collapsed={collapsed} />
                 </Header>
                 <Content  style={{ padding: 24, minHeight: '100vh', }}   className={collapsed ? "collapsed mainContnet " : "mainContnet"}>
-                    {props.children}
+                    {children}
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>Ant Design © {new Date().getFullYear()} Created by Ant UED</Footer>
             </Layout>
@@ -38,3 +44,4 @@ export default function Layouts(props){
     );
 }
 
+export default Layouts;
